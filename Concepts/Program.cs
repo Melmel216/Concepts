2598604985f57b63f3f1e8505ee25878caec07e7
Melmel216 <168171266+Melmel216@users.noreply.github.com>
2024-06-25 11:18:54 +0200

    Projekt für Todo-App erstellt

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -1,2 +1,50 @@
 ﻿Console.WriteLine("Hello, World!");
 
+/*
+ PascalCase => WorttrennungenWerdenGroßgeschrieben (alle Anfangsbuchstaben groß)
+ camelcase => worttrennungenWerdenGroßgeschrieben (erster Anfangsbuchstabe klein, die folgenden groß)
+ snake_case => worttrennungen_werden_kleingeschrieben
+ kebap-case => worttrennungen-werden-kleingeschrieben
+ */
+
+
+/*
+ DateTime ist einer von vielen komplexen Datentypen in C#.
+DateTime ist ein Strukturtyp, der Datum und Uhrzeit darstellt.
+Komplexe Datentypen sind Objekte, die aus mehreren Werten bestehen, welche
+ebenfalls komplex oder auch primitiv sein können. 
+Objekte können nur in der Konsole ausgegeben werden, wenn sie eine
+Methode ToString() haben.
+ */
+
+DateTime date = DateTime.Now;
+
+Console.WriteLine(date.Year);
+
+
+///////Funktionen
+
+
+Console.WriteLine();
+
+
+//Eigene Funktion einführen:
+void PrintName()
+{
+    Console.WriteLine("Hello World!");
+}
+
+//Eigene Funktion aufrufen:
+PrintName();
+PrintName();
+PrintName();
+
+string GetGreeting()
+{
+    return "Herzlich Willkommen!";
+}
+
+var greeting = GetGreeting();
+
+Console.WriteLine(greeting);
+
