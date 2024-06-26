5819afa049500dadafd5ceaaf6e2a9232cd2d669
Melmel216 <168171266+Melmel216@users.noreply.github.com>
2024-06-26 13:05:39 +0200

    Formatierung

--- TD/Program.cs
+++ TD/Program.cs
@@ -30,22 +30,27 @@ do
             break;
         case "2":
             Console.WriteLine("Du hast folgende Aufgaben:");
+            
             for (int i = 0; i < todos.Length; i++)
             {
                 Console.WriteLine($"\t{i}. {todos[i]}");
             }
+            
             Console.WriteLine("Drücke <Enter> um zurück ins Menü zu kommen.");
             Console.ReadKey();
+            
             break;
         case "3":
             //Löschen
             Console.WriteLine("Welches Todo möchtest du löschen?");
+            
             for (int i = 0; i < todos.Length; i++)
             {
                 Console.WriteLine($"\t{i}. {todos[i]}");
             }
             var todoIndexString = Console.ReadLine();
             int todoIndex = int.Parse(todoIndexString);
+            
             todos[todoIndex] = null;
             
             Console.WriteLine("Todo erfolgreich gelöscht.");
@@ -54,6 +59,7 @@ do
             //Updaten eines Todos (verändern des strings)
             //
             Console.WriteLine("Welches Todo möchtest du updaten?");
+            
             for (int i = 0; i < todos.Length; i++)
             {
                 Console.WriteLine($"\t{i}. {todos[i]}");
@@ -61,11 +67,11 @@ do
             todoIndexString = Console.ReadLine();
             todoIndex = int.Parse(todoIndexString);
             
-            Console.WriteLine("Bitte gebe das neue Todo ein:");
+            Console.WriteLine("Bitte gebe die Änderung ein:");
             var todoupdate = Console.ReadLine();
             todos[todoIndex] = todoupdate;
             
-            Console.WriteLine("Todo-Update erfolgreich:" + todo[todoIndex]);
+            Console.WriteLine("Todo-Update erfolgreich:" + todos[todoIndex]);
             break;
     }
 } while (true);
