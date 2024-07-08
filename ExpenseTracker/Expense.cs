4dab234b414590fa72a29a5940149d3f488bc8b4
Melmel216 <168171266+Melmel216@users.noreply.github.com>
2024-07-08 15:40:30 +0200

    Expenses Case 1

---
+++ ExpenseTracker/Expense.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpenseTracker
+{
+    internal class Expense
+    {
+        //Properties
+        internal string Name { get; set; }
+        internal decimal Price { get; set; }
+        internal string Category { get; set; }
+        internal DateTime CreatedAt { get; set; }
+
+        public Expense(string name, decimal price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            CreatedAt = DateTime.Now;
+        }
+
+    }
+}
