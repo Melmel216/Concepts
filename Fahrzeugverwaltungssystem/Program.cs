df95452a9051c730a08000372fd462e9bee0a357
Melmel216 <168171266+Melmel216@users.noreply.github.com>
2024-07-09 14:39:43 +0200

    Leerzeichen

--- Fahrzeugverwaltungssystem/Program.cs
+++ Fahrzeugverwaltungssystem/Program.cs
@@ -13,15 +13,15 @@ Car car3 = new Car("BMW", "X5", 2019, 32000);
 int drivenKm = 2000;
 //Car1
 Console.WriteLine(car1.GetDescription());
-Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car1.Drive(drivenKm) + "km");
+Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car1.Drive(drivenKm) + " km");
 Console.WriteLine("Das Auto ist " + car1.Age() + " Jahre alt.\n");
 
 //Car2
 Console.WriteLine(car2.GetDescription());
-Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car2.Drive(drivenKm) + "km");
+Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car2.Drive(drivenKm) + " km");
 Console.WriteLine("Das Auto ist " + car2.Age() + " Jahre alt.\n");
 
 //Car3
 Console.WriteLine(car3.GetDescription());
-Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car3.Drive(drivenKm) + "km");
+Console.WriteLine("Nach " + drivenKm + " weiteren gefahrenen Kilometern\nbeträgt der neue Kilometerstand: " + car3.Drive(drivenKm) + " km");
 Console.WriteLine("Das Auto ist " + car3.Age() + " Jahre alt.\n");
