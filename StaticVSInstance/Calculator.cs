b0da1c86725316592ab880723f11f4306c11bdf2
Melmel216 <168171266+Melmel216@users.noreply.github.com>
2024-07-10 15:48:17 +0200

    Calculator

--- StaticVSInstance/Calculator.cs
+++ StaticVSInstance/Calculator.cs
@@ -8,5 +8,93 @@ namespace StaticVSInstance
 {
     internal class Calculator
     {
+        //Keine Properties
+        // Properties
+        // public string Title { get; set; }
+        // public int InstanceCount { get; set; }
+        public static int Count { get; set; }
+
+
+        // Methoden
+        public static int GetNumberInput(string inputPrompt)
+        {
+            Console.Write(inputPrompt);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int Add(int numberInput1, int numberInput2)
+        {
+            Count++;
+            var result = numberInput1 + numberInput2;
+            Console.WriteLine($"Das Ergebnis von {numberInput1} + {numberInput2} = {result}");
+            Console.WriteLine($"Du hast {Count} Berechnungen getätigt.");
+            return result;
+        }
+
+        public static int Subtract(int numberInput1, int numberInput2)
+        {
+            Count++;
+            var result = numberInput1 - numberInput2;
+            Console.WriteLine($"Das Ergebnis von {numberInput1} - {numberInput2} = {result}");
+            Console.WriteLine($"Du hast {Count} Berechnungen getätigt.");
+            return result;
+        }
+
+        public static int Multiply(int numberInput1, int numberInput2)
+        {
+            Count++;
+            var result = numberInput1 * numberInput2;
+            Console.WriteLine($"Das Ergebnis von {numberInput1} * {numberInput2} = {result}");
+            Console.WriteLine($"Du hast {Count} Berechnungen getätigt.");
+            return result;
+        }
+
+        public static int Divide(int numberInput1, int numberInput2)
+        {
+            Count++;
+            var result = numberInput1 / numberInput2;
+            Console.WriteLine($"Das Ergebnis von {numberInput1} / {numberInput2} = {result}");
+            Console.WriteLine($"Du hast {Count} Berechnungen getätigt.");
+            return result;
+        }
+
+        /// <summary>
+        /// Das hier ist wie Add(), aber wird über die Instanz der Klasse (konkretes Objekt eg. "calc1.AddInstance") aufgerufen. Der InstanceCount ist für jede Instanz (Objekt) unterschiedlich (eg. calc1.InstanceCount != calc2.InstanceCount)
+        /// </summary>
+        /// <param name="numberInput1"></param>
+        /// <param name="numberInput2"></param>
+        /// <returns></returns>
+        /*
+        public int AddInstance(int numberInput1, int numberInput2)
+        {
+          InstanceCount++;
+          var result = numberInput1 + numberInput2;
+          Console.WriteLine($"Das Ergebnis von {numberInput1} + {numberInput2} = {result}");
+          return result;
+        }
+        */
+
+        //eigene Methoden
+        internal static int GetInput(string text)
+        {
+            Console.WriteLine(text);
+            int numberInput = int.Parse(Console.ReadLine());
+            return numberInput;
+        }
+
+        internal static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Was willst du rechnen?");
+            Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide");
+        }
+
+        internal static void BackToMenu()
+        {
+            Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
+            Console.ReadKey();
+        }
+
+
     }
 }
--- StaticVSInstance/Program.cs
+++ StaticVSInstance/Program.cs
@@ -1,53 +1,47 @@
-﻿int numberInput1 = 0;
+﻿using StaticVSInstance;
+
+int numberInput1 = 0;
 int numberInput2 = 0;
 int result = 0;
 
 do
 {
-    Console.Clear();
-    Console.WriteLine("Was willst du rechnen?");
-    Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide");
+    //Console.Clear();
+    //Console.WriteLine("Was willst du rechnen?");
+    //Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide");
+    Calculator.ShowMenu();
     var menuInput = Console.ReadLine();
     switch (menuInput)
     {
         case "1":
-            Console.Write("Gebe die erste Zahl ein:\t");
-            numberInput1 = int.Parse(Console.ReadLine());
-            Console.Write("Gebe die zweite Zahl ein:\t");
-            numberInput2 = int.Parse(Console.ReadLine());
+            //Calculator.
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\t");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\t");
             // var numberInput1 = Calculator.GetInput();
-            result = numberInput1 + numberInput2;
-            Console.WriteLine($"Das Ergebnis von {numberInput1} + {numberInput2} = {result}");
+            Calculator.Add(numberInput1, numberInput2);
             Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
             Console.ReadKey();
+            //Calculator.BackToMenu();
             break;
         case "2":
-            Console.Write("Gebe die erste Zahl ein:\t");
-            numberInput1 = int.Parse(Console.ReadLine());
-            Console.Write("Gebe die zweite Zahl ein:\t");
-            numberInput2 = int.Parse(Console.ReadLine());
-            result = numberInput1 - numberInput2;
-            Console.WriteLine($"Das Ergebnis von {numberInput1} - {numberInput2} = {result}");
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\t");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\t");
+            Calculator.Subtract(numberInput1, numberInput2);
             Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
             Console.ReadKey();
             break;
         case"3":
-            Console.Write("Gebe die erste Zahl ein:\t");
-            numberInput1 = int.Parse(Console.ReadLine());
-            Console.Write("Gebe die zweite Zahl ein:\t");
-            numberInput2 = int.Parse(Console.ReadLine());
-            result = numberInput1 * numberInput2;
-            Console.WriteLine($"Das Ergebnis von {numberInput1} * {numberInput2} = {result}");
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\t");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\t");
+            Calculator.Multiply(numberInput1, numberInput2); 
             Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
             Console.ReadKey();
             break;
         case "4":
-            Console.Write("Gebe die erste Zahl ein:\t");
-            numberInput1 = int.Parse(Console.ReadLine());
-            Console.Write("Gebe die zweite Zahl ein:\t");
-            numberInput2 = int.Parse(Console.ReadLine());
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\t");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\t");
             result = numberInput1 / numberInput2;
-            Console.WriteLine($"Das Ergebnis von {numberInput1} / {numberInput2} = {result}");
+            Calculator.Divide(numberInput1, numberInput2);
             Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
             Console.ReadKey();
             break;
